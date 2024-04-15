f7cf577e484f2cb042ab229541c197ef454f4341
Krystian Piszczela <01168907@pw.edu.pl>
2024-04-15 14:45:53 +0200

    lekcja 10

---
+++ gra_snake/Segment.py
@@ -0,0 +1,13 @@
+import pygame
+import copy
+
+class Segment(pygame.sprite.Sprite):
+    def __init__(self, kolor, pozycja):
+        super().__init__()
+        self.obraz = pygame.Surface([32, 32])
+        self.pozycja = pygame.Rect(-32, -32, 32, 32)
+        self.ostatania_pozycja = None
+
+    def przesun(self, nowa_pozycja):
+        self.ostatania_pozycja = copy.deepcopy(self.pozycja)
+        self.pozycja = copy.deepcopy(nowa_pozycja)
